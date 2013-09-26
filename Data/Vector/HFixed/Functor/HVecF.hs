7a14d2b37bba0e4f372e64b41e457e4fcb286a6a
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2013-09-26 19:59:52 +0400

    Add HVecF

---
+++ Data/Vector/HFixed/Functor/HVecF.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+-- |
+module Data.Vector.HFixed.Functor.HVecF (
+    HVecF(..)
+  ) where
+
+import Data.Vector.HFixed.Cont
+import Data.Vector.HFixed.Functor.Class
+import Data.Vector.HFixed.HVec (HVec)
+
+-- | Partially heterogeneous vector which can hold elements of any
+--   type.
+newtype HVecF xs f = HVecF { getHVecF :: HVec (Wrap f xs) }
+
+instance Arity xs => HVector (HVecF xs f) where
+  type Elems (HVecF xs f) = Wrap f xs
+  inspect v f = inspectF v (castFun f)
+  construct = castTFun constructF
+  {-# INLINE inspect   #-}
+  {-# INLINE construct #-}
+
+-- castWrapped here is used to remove constraints like:
+-- `Arity (Wrap f xs)' and to replace them with `Arity xs'
+instance Arity xs => HVectorF (HVecF xs) where
+  type ElemsF (HVecF xs) = xs
+  inspectF (HVecF v) (f :: TFun f xs a)
+    = ( unInspect $ castWrapped
+        (Inspect inspect :: Arity (Wrap f xs) => Inspect a f xs)
+      ) v (castTFun f)
+  constructF
+    = castFun $ unWrapFun $ castWrapped
+    ( WrapFun $ fmap HVecF construct :: Arity (Wrap f xs) => WrapFun (HVecF xs f) f xs)
+  {-# INLINE inspectF   #-}
+  {-# INLINE constructF #-}
+
+newtype WrapFun r f xs = WrapFun { unWrapFun :: Fun (Wrap f xs) r }
+newtype Inspect a f xs = Inspect { unInspect :: HVec (Wrap f xs) -> Fun (Wrap f xs) a -> a }
