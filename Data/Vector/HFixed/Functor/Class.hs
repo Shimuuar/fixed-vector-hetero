f4829f064e36abf761980e1d0f8c6e1bdf5dd8fd
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2013-09-26 17:45:09 +0400

    Add combinators for TFun and vector type class

--- Data/Vector/HFixed/Functor/Class.hs
+++ Data/Vector/HFixed/Functor/Class.hs
@@ -1,26 +1,32 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE KindSignatures        #-}
 {-# LANGUAGE TypeOperators         #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE DefaultSignatures     #-}
-{-# LANGUAGE UndecidableInstances  #-}
 {-# LANGUAGE PolyKinds             #-}
 {-# LANGUAGE RankNTypes            #-}
-{-# LANGUAGE ConstraintKinds       #-}
-{-# LANGUAGE InstanceSigs          #-}
 module Data.Vector.HFixed.Functor.Class (
     -- * Types
     TFun(..)
   , castFun
   , castTFun
+  , HVectorF(..)
+    -- * Combinators
+  , curryTFun
+  , uncurryTFun
+  , uncurryTFun2
+  , shuffleTF
   ) where
 
 import Control.Applicative (Applicative(..))
 import Data.Vector.HFixed.Class
+
+
+
+----------------------------------------------------------------
+-- Types and type classes
+----------------------------------------------------------------
 
 -- | Newtype wrapper for function where all type parameters have same
 --   type constructor
@@ -34,6 +40,51 @@ castFun = TFun . unFun
 castTFun :: TFun f xs b -> Fun (Wrap f xs) b
 castTFun = Fun . unTFun
 {-# INLINE castTFun #-}
+
+
+-- | Type class for partially homogeneous vector. Every type in the
+--   vector have same type constructor.
+class HVectorF v where
+  -- | Elements of the vector without type constructors
+  type ElemsF v :: [*]
+  inspectF   :: v f -> TFun f (ElemsF v) a -> a
+  constructF :: TFun f (ElemsF v) (v f)
+
+
+
+----------------------------------------------------------------
+-- Combinators
+----------------------------------------------------------------
+
+-- | Apply single parameter to function
+curryTFun :: TFun f (x ': xs) r -> f x -> TFun f xs r
+curryTFun (TFun f) = TFun . f
+{-# INLINE curryTFun #-}
+
+-- | Uncurry single parameter
+uncurryTFun :: (f x -> TFun f xs r) -> TFun f (x ': xs) r
+uncurryTFun = TFun . fmap unTFun
+{-# INLINE uncurryTFun #-}
+
+-- | Uncurry two parameters for nested TFun.
+uncurryTFun2 :: (Arity xs, Arity ys)
+             => (f x -> f y -> TFun f xs (TFun f ys r))
+             -> TFun f (x ': xs) (TFun f (y ': ys) r)
+uncurryTFun2 = uncurryTFun . fmap (fmap uncurryTFun . shuffleTF . uncurryTFun)
+{-# INLINE uncurryTFun2 #-}
+
+
+-- | Move first argument of function to its result. This function is
+--   useful for implementation of lens.
+shuffleTF :: forall f x xs r. Arity xs => TFun f (x ': xs) r -> TFun f xs (f x -> r)
+{-# INLINE shuffleTF #-}
+shuffleTF (TFun f0) = TFun $ accumTy
+  (\(T_shuffle f) a -> T_shuffle (\x -> f x a))
+  (\(T_shuffle f)   -> f)
+  (T_shuffle f0 :: T_shuffle f x r xs)
+
+data T_shuffle f x r xs = T_shuffle (Fn (Wrap f (x ': xs)) r)
+
 
 
 ----------------------------------------------------------------
