3da28e61138eb1e1e7b518b4374dfb5be3098078
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2017-10-31 19:43:06 +0300

    Use closed type families

--- Data/Vector/HFixed/TypeFuns.hs
+++ Data/Vector/HFixed/TypeFuns.hs
@@ -24,32 +24,32 @@ proxy _ = Proxy
 
 
 -- | Concaternation of type level lists.
-type family   (++) (xs :: [α]) (ys :: [α]) :: [α]
-type instance (++) '[]      ys = ys
-type instance (++) (x : xs) ys = x : xs ++ ys
+type family   (++) (xs :: [α]) (ys :: [α]) :: [α] where
+  (++) '[]      ys = ys
+  (++) (x : xs) ys = x : xs ++ ys
 
 
 -- | Length of type list expressed as type level naturals from
 --   @fixed-vector@.
-type family   Len (xs :: [α]) :: *
-type instance Len '[]      = Z
-type instance Len (x : xs) = S (Len xs)
+type family   Len (xs :: [α]) where
+  Len '[]      = Z
+  Len (x : xs) = S (Len xs)
 
 -- | Head of type list
-type family   Head (xs :: [α]) :: α
-type instance Head (x : xs) = x
+type family   Head (xs :: [α]) :: α where
+  Head (x : xs) = x
 
 
 -- | Homogeneous type list with length /n/ and element of type /a/. It
 --   uses type level natural defined in @fixed-vector@.
-type family   HomList n (a :: α) :: [α]
-type instance HomList  Z    a = '[]
-type instance HomList (S n) a = a : HomList n a
+type family   HomList n (a :: α) :: [α] where
+  HomList  Z    a = '[]
+  HomList (S n) a = a : HomList n a
 
 -- | Wrap every element of list into type constructor
-type family   Wrap (f :: α -> β) (a :: [α]) :: [β]
-type instance Wrap f '[]      = '[]
-type instance Wrap f (x : xs) = (f x) : (Wrap f xs)
+type family   Wrap (f :: α -> β) (a :: [α]) :: [β] where
+  Wrap f '[]      = '[]
+  Wrap f (x : xs) = (f x) : (Wrap f xs)
 
 
 
