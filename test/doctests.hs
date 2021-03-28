1ec39fa3f90743095264b4fd8bfaca511bbe559a
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2021-03-28 17:48:00 +0300

    Try to switch to plain doctests without cabal-doctest

--- test/doctests.hs
+++ test/doctests.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import Build_doctests (flags, pkgs, module_sources)
 import Data.Foldable  (traverse_)
 import Test.DocTest   (doctest)
 
@@ -9,4 +8,5 @@ main = do
     traverse_ putStrLn args
     doctest args
   where
-    args = flags ++ pkgs ++ module_sources
+    args = ["Data"]
+
